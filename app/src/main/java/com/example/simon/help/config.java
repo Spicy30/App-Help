d587f1af5f674bbf607c5a7ce5ffc2ec07606dea
jim841019g <jim841019g@gmail.com>
2016-06-20 15:13:32 +0800

    maiupdate

--- app/src/main/java/com/example/simon/help/config.java
+++ app/src/main/java/com/example/simon/help/config.java
@@ -4,5 +4,5 @@ package com.example.simon.help;
  * Created by william on 6/17/16.
  */
 public class config {
-    public static final String server_ip = "140.112.249.22";
+    public static final String server_ip = "10.5.2.50";
 }
